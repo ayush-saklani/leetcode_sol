d6066c5d7dfc715bf407d5085f6277d383842e6d
“ayush <ayushsaklani04@gmail.com>
2024-03-04 10:42:02 +0530

    04-03-2024

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,79 +1,85 @@
-#include <iostream>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
-
-int calculate_size(int n){
-    int x = ceil(log2(n));
-    int size =  2*pow(2, x) - 1;
-    return size;
-}
-int* init(int size){
-    int *temp = new int[size];
-    return temp;
-}
-void buildTree(int *segment_tree, int *arr, int sl, int sr, int i){
-    if(sl == sr){
-        segment_tree[i] = arr[sl];
-        return;
+void build_segment_tree(int*arr,int* seg_tree,int l,int r,int tree_index,int n){
+    if(l==r){
+        seg_tree[tree_index] = 0;
+        if(l<n)
+            seg_tree[tree_index] = arr[l];
+        return ;
     }
-    int mid = sl + (sr-sl)/2;
-    buildTree(segment_tree, arr, sl, mid, 2*i+1);
-    buildTree(segment_tree, arr, mid+1, sr, 2*i+2);
-    segment_tree[i] = segment_tree[2*i+1] + segment_tree[2*i+2];
-    return;
-}
-void update_utils(int *segment_tree, int sl, int sr, int curI, int index, int key, int changeValue){
-    if(sl == sr){
-        segment_tree[curI] = key;
-        return;
+    else{
+        int mid = (l+r)/2;
+        build_segment_tree(arr,seg_tree,l,mid,(tree_index*2+1),n);
+        build_segment_tree(arr,seg_tree,mid+1,r,(tree_index*2+2),n);
     }
-    segment_tree[curI] += changeValue;
-    int mid = sl + (sr - sl)/2;
-
-    if(index <= mid )
-        update_utils(segment_tree, sl, mid, 2*curI+1, index, key, changeValue);
-    else
-        update_utils(segment_tree, mid+1, sr, 2*curI+2, index, key, changeValue);
+    seg_tree[tree_index] = seg_tree[tree_index*2+1] + seg_tree[tree_index*2+2];
 }
-void update(int *arr, int *segment_tree, int n, int index, int key){
-    int changeValue = key - arr[index];
-    arr[index] = key;
-    update_utils(segment_tree, 0, n-1, 0, index, key, changeValue);
+int sum_query(int* seg_tree,int l,int r,int ql,int qr,int tree_index){
+    int mid= (l+r)/2;
+    // cout<<endl<<l<<" "<<mid<<" "<<r;
+    if(qr<l || ql>r) return 0;
+    if(ql<=l && qr>=r){
+        return  seg_tree[tree_index];
+    }
+    int temp_l = sum_query(seg_tree,l,mid,ql,qr,2*tree_index+1);
+    int temp_r = sum_query(seg_tree,mid+1,r,ql,qr,2*tree_index+2);
+    return  temp_l+temp_r;
 }
-int calculateSum(int *arr, int *segment_tree, int sl, int sr, int index, int ql, int qr){
-    if(sr < ql || sl > qr) return 0;
-    if(sl >= ql && sr <= qr) return segment_tree[index];
-    int mid = sl + (sr-sl)/2;
-    return  calculateSum(arr, segment_tree, sl, mid, 2*index+1, ql, qr)+
-            calculateSum(arr, segment_tree, mid+1, sr, 2*index+2, ql, qr);
+void update_query(int* seg_tree,int l,int r,int upd_index,int tree_index,int val_p_diff){
+    if(upd_index<l || upd_index>r) return ;
+    if(upd_index>=l && upd_index<=r)
+        seg_tree[tree_index]+=val_p_diff;
+    if(l==r)
+        return;
+    int mid= (l+r)/2;
+    update_query(seg_tree,l,mid,upd_index,2*tree_index+1,val_p_diff);
+    update_query(seg_tree,mid+1,r,upd_index,2*tree_index+2,val_p_diff);
+    return ;
+}
+void print_seg_tree(int* seg_tree,int seg_tree_size){
+    for(int i = 0; i < seg_tree_size;i++){
+        cout<<seg_tree[i]<<" ";
+    }
+    cout<<endl;
 }
 int main(){
-    int n, index, key, l, r;
+    int n;
     cin>>n;
     int* arr = new int[n];
     for (int i = 0; i < n; i++){
         cin>>arr[i];
     }
-    int segmentSize = calculate_size(n);
-    int *segment_tree = init(segmentSize);
-    buildTree(segment_tree, arr, 0, n-1, 0);
-    while(1){
+    int seg_tree_size=2*pow(2, ceil(log2(n)))-1;
+    int* seg_tree = new int[seg_tree_size];
+    build_segment_tree(arr,seg_tree,0,pow(2, ceil(log2(n)))-1,0,n); //pow(2, ceil(log2(n))) means last layer ko fully full karne ke baad kitne element ayenge
+    print_seg_tree(seg_tree,seg_tree_size);
+    cout<<"press 1 for range sum"<<endl;
+    cout<<"press 2 for update at index"<<endl;
+    while (1){
         int choice;
-		cin>>choice;
-		switch(choice){
-        case 1: 
-            cin>>index>>key;
-			update(arr, segment_tree, n, index, key);
-			break;
-		
-		case 2:
-            cin>>l>>r;
-			cout<<"Sum = "<<calculateSum(arr, segment_tree, 0, n-1, 0, l, r)<<endl;
-			break;
+        cin>>choice;
+        switch (choice){
+        case 1:
+            int start,end;
+            cout<<"start -> end ";
+            cin>>start>>end;
+            cout<<sum_query(seg_tree,0,pow(2, ceil(log2(n)))-1,start,end,0);
+            break;
+        case 2:
+            int index,value;
+            cout<<"index -> value ";
+            cin>>index>>value;
+            update_query(seg_tree,0,pow(2, ceil(log2(n)))-1,index,0,value-arr[index]);
+            cout<<"updation complete "<<endl;
+            print_seg_tree(seg_tree,seg_tree_size);
+            break;        
         default:
-            cout<<"wrong choice";
+            cout<<"invalid choice"<<endl;
+            cout<<"Exiting....."<<endl;
             return 0;
-		}
-	}
+            break;
+        }
+    }
     return 0;
 }
+//  5 1 2 3 4 5 2 0 3
