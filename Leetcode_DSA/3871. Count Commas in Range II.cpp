aeb19594ace8205ce2e1368fa8fca45910727558
Ayush Saklani <113051005+ayush-saklani@users.noreply.github.com>
2026-09-11 15:47:51 +0530

    ss

--- Leetcode_DSA/3871. Count Commas in Range II.cpp
+++ Leetcode_DSA/3871. Count Commas in Range II.cpp
@@ -9,7 +9,9 @@ public:
         if (n >= 1000000000000)     res = res + (n - 999999999999);
         if (n >= 1000000000000000)  res = res + (n - 999999999999999);
         // sorrry 🤭
+        // thora complex hora tha toh, i saw 15 digit limit and hardcoded the values,
+        //  but i think this is not a better approach if you see this problem in future, 
+        // so i will try to make it better with a loop and a formula, but for now this is the solution.
         return res;
-
     }
 };
